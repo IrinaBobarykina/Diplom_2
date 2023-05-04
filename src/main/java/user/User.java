0c0e437409b8be6ea7d55cc820534080ba494553
IrinaBobarykina <ira.bobarykina5@gmail.com>
2023-05-04 22:03:22 +0400

    Added UserOperations.java

--- src/main/java/user/User.java
+++ src/main/java/user/User.java
@@ -1,2 +1,42 @@
-package user;public class User {
+package user;
+
+public class User {
+    private String email;
+    private String password;
+    private String name;
+
+    public User(String email, String password, String name) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+    }
+
+    public User(String password, String name) {
+        this.password = password;
+        this.name = name;
+    }
+
+    public String getLogin() {
+        return email;
+    }
+
+    public void setLogin(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/user/UserOperations.java
+++ src/main/java/user/UserOperations.java
@@ -1,2 +1,41 @@
-package user;public class UserOperations {
+package user;
+
+import io.qameta.allure.Step;
+import io.restassured.response.Response;
+import utils.APIs;
+
+import static io.restassured.RestAssured.given;
+
+public class UserOperations {
+
+    @Step("Create an user")
+    public static Response createUser(User user) {
+        Response response = given()
+                .header("Content-type", "application/json")
+                .and()
+                .body(user)
+                .when()
+                .post(APIs.REGISTER_PATH);
+        return response;
+    }
+
+    @Step("Sign in an user")
+    public static Response signInUser(User user) {
+        Response response = given()
+                .header("Content-type", "application/json")
+                .and()
+                .body(user)
+                .when()
+                .post(APIs.LOGIN_PATH);
+        return response;
+    }
+
+    @Step("Delete an user")
+    public static void deleteUser(String accessToken) {
+        if (accessToken != null)
+            given()
+                    .header("Authorization", accessToken)
+                    .when()
+                    .delete(APIs.USER_PATH);
+    }
 }

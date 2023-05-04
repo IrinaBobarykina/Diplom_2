3d5b19ce7ec0950540ad11a3b039934993bf721e
IrinaBobarykina <ira.bobarykina5@gmail.com>
2023-05-04 17:54:56 +0400

    Added APIs for tests

--- src/main/java/utils/APIs.java
+++ src/main/java/utils/APIs.java
@@ -1,2 +1,17 @@
-package utils;public class APIs {
+package utils;
+
+public class APIs {
+
+//    создание юзера POSt
+public static final String REGISTER_PATH = "/api/auth/register";
+
+//    логин юзера POST
+public static final String LOGIN_PATH = "/api/auth/login ";
+
+//    изменение данных юзера PATCH + удаление пользователя DELETE
+public static final String USER_PATH = "/api/auth/user";
+
+//    создание заказа POST + получение заказов пользователя GET
+public static final String ORDERS_PATH = "/api/orders";
+
 }

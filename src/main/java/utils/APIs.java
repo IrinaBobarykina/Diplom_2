7f29b018e68667e8eb6c4c2e23dbb00319101ac4
IrinaBobarykina <ira.bobarykina5@gmail.com>
2023-05-08 23:20:58 +0400

    Added API for ingredients

--- src/main/java/utils/APIs.java
+++ src/main/java/utils/APIs.java
@@ -2,16 +2,19 @@ package utils;
 
 public class APIs {
 
-//    создание юзера POST
-public static final String REGISTER_PATH = "/api/auth/register";
+    //    создание юзера POST
+    public static final String REGISTER_PATH = "/api/auth/register";
 
-//    логин юзера POST
-public static final String LOGIN_PATH = "/api/auth/login ";
+    //    логин юзера POST
+    public static final String LOGIN_PATH = "/api/auth/login ";
 
-//    изменение данных юзера PATCH + удаление пользователя DELETE
-public static final String USER_PATH = "/api/auth/user";
+    //    изменение данных юзера PATCH + удаление пользователя DELETE
+    public static final String USER_PATH = "/api/auth/user";
 
-//    создание заказа POST + получение заказов пользователя GET
-public static final String ORDERS_PATH = "/api/orders";
+    //    создание заказа POST + получение заказов пользователя GET
+    public static final String ORDERS_PATH = "/api/orders";
+
+    //    получение всех ингредиентов GET
+    public static final String INGREDIENTS_PATH = "/api/ingredients";
 
 }
